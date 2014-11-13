cd47e06f94bb5021cb9a0a712bb8a0d14021ac59
vemulap <prathyusha.vemula@gmail.com>
2014-11-13 01:15:16 -0800

    new changes for form

--- js/cpconfig.js
+++ js/cpconfig.js
@@ -1,6 +1,53 @@
 /**
  * Created by pvemula on 11/12/14.
  */
-$("#myselect").on("change", function() {
-    $("#" + $(this).val()).show().siblings().hide();
-})
+
+
+$( document ).ready(function() {
+
+    $(".btn-create").on('click', function (event) {
+
+
+    var formData = {
+        'cpName'                 : $('input[name=cpName]').val(),
+        'feedFormat'             : $('.feed-format').val(),
+        'superheroAlias'         : $('input[name=adapter]').val(),
+        'dataDestination'        : $('.data-destination').val()
+            };
+
+        alert(JSON.stringify(formData));
+
+
+    });
+
+
+
+
+
+
+
+
+        $('#option1, #option2, #option3, #option4, #option5, #option6, #option7, #option8').hide();
+
+        $('#dataDestination').change(function() {
+            $('#option1, #option2, #option3, #option4, #option5, #option6, #option7, #option8').hide();
+            $('#option' + $(this).find('option:selected').attr('id')).show();
+
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
